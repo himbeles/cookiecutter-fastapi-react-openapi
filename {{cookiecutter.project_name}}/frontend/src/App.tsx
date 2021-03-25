eb8a7055f84b4e81b6ee642d906f7207cd0bf5aa
Luis Riegger <lri@me.com>
2021-03-25 15:45:31 +0100

    fix app.tsx

--- {{cookiecutter.project_name}}/frontend/src/App.tsx
+++ {{cookiecutter.project_name}}/frontend/src/App.tsx
@@ -1,42 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Configuration, DefaultApi, Item} from './api'
+import React, { useState } from "react";
+import "./App.css";
+import { Configuration, DefaultApi, Item } from "./api";
+import { Button } from "@chakra-ui/react";
 
-const config = new Configuration({ basePath: 'http://localhost:8000' }); // TODO: This is for dev
+const config = new Configuration({ basePath: "http://localhost:8000" }); // TODO: This is for dev
 export const apiClient = new DefaultApi(config);
 
 function App() {
-
   const [item, setItem] = useState<Item>();
 
-
-  useEffect(() => {
-    apiClient.readItem(1,"my test item from api").then((response) => {
+  function updateItem() {
+    apiClient.readItem(1, "my test item from api").then((response) => {
       setItem(response.data);
-      console.log(response.data)
-    })
-  })
+      console.log(response.data);
+    });
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Item #{item?.id}: {item?.description ?? "no item available"}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button
+        onClick={updateItem}
+        loadingText="Get Item"
+        colorScheme="teal"
+        variant="outline"
+      >
+        Refresh
+      </Button>
+      <p>
+        Item #{item?.id}: {item?.description ?? "no item available"}
+      </p>
     </div>
   );
 }
